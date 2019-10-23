9cc763ead613b16a48fa85a6deba933fc2bc7bf7
Olasubulumi Ogo-Oluwa Jesutomi <33330651+tomilashy@users.noreply.github.com>
2019-10-23 14:51:37 -0400

    credits page done

--- Programming/app/src/main/java/com/project/coen_elec_390/CreditsActivity.java
+++ Programming/app/src/main/java/com/project/coen_elec_390/CreditsActivity.java
@@ -2,63 +2,58 @@ package com.project.coen_elec_390;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.ObjectAnimator;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
-import android.view.animation.Animation;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CreditsActivity extends AppCompatActivity {
 
-    private ImageView drop1;
-    private ImageView drop2;
-    private ImageView drop3;
+    ArrayAdapter<String> shadiAdapter,alecAdapter,irfanAdapter,tomiAdapter,cvuAdapter;
+    ArrayAdapter adapter2;
+    List<String> shadiList = new ArrayList<>();
+    List<String> alecList = new ArrayList<>();
+    List<String> irfanList = new ArrayList<>();
+    List<String> tomiList = new ArrayList<>();
+    List<String> cvuList = new ArrayList<>();
+    ListView shadiListView,alecListView,irfanListView,tomiListView,cvuListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+        Log.d("Etienne is bawss","in credits");
+        shadiList.addAll(Arrays.asList(new String[]{"Project Leader", "Shadi Makdissi"}));
+        alecList.addAll(Arrays.asList(new String[]{"Developer", "Shadi Makdissi"}));
+        irfanList.addAll(Arrays.asList(new String[]{"Developer", "Shadi Makdissi"}));
+        tomiList.addAll(Arrays.asList(new String[]{"Developer", "Shadi Makdissi"}));
+        cvuList.addAll(Arrays.asList(new String[]{"Developer", "Shadi Makdissi"}));
+//        setting adapter
+        shadiAdapter = new ArrayAdapter<String>(this, R.layout.listview2, shadiList);
+        alecAdapter = new ArrayAdapter<String>(this, R.layout.listview2, alecList);
+        irfanAdapter = new ArrayAdapter<String>(this, R.layout.listview2, irfanList);
+        tomiAdapter = new ArrayAdapter<String>(this, R.layout.listview2, tomiList);
+        cvuAdapter = new ArrayAdapter<String>(this, R.layout.listview2, cvuList);
+//        displaying list view
 
-        drop1 = findViewById(R.id.drop1);
-        drop2 = findViewById(R.id.drop2);
-        drop3 = findViewById(R.id.drop3);
+        shadiListView = findViewById(R.id.shadiListview);
+        shadiListView.setAdapter(shadiAdapter);
+        alecListView = findViewById(R.id.alecListview);
+        alecListView.setAdapter(alecAdapter);
+        irfanListView = findViewById(R.id.irfanListview);
+        irfanListView.setAdapter(irfanAdapter);
+        tomiListView = findViewById(R.id.tomiListview);
+        tomiListView.setAdapter(tomiAdapter);
+        cvuListView = findViewById(R.id.cvuListview);
+        cvuListView.setAdapter(cvuAdapter);
 
-        animateSweat();
-    }
 
-    private void animateSweat() {
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-
-        float heightResolution = size.y;
-        float pivotResolution = 2028f;
-
-        float translationY1 = (500f / pivotResolution) * heightResolution;
-        float translationY2 = (350f / pivotResolution) * heightResolution;
-        float translationY3 = (150f / pivotResolution) * heightResolution;
-
-        Log.d("CreditsActivity", "heightResolution =  " + heightResolution);
-
-        ObjectAnimator animation1 = ObjectAnimator.ofFloat(drop1, "translationY", translationY1);
-        ObjectAnimator animation2 = ObjectAnimator.ofFloat(drop2, "translationY", translationY2);
-        ObjectAnimator animation3 = ObjectAnimator.ofFloat(drop3, "translationY", translationY3);
-
-        animation1.setDuration(2000);
-        animation2.setDuration(2000);
-        animation3.setDuration(2000);
-
-        animation1.setRepeatCount(Animation.INFINITE);
-        animation2.setRepeatCount(Animation.INFINITE);
-        animation3.setRepeatCount(Animation.INFINITE);
-
-        animation1.start();
-        animation2.start();
-        animation3.start();
 
     }
+
+
 }
