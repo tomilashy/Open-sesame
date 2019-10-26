ae2b06922c841302df4a72427f778fef987acbeb
cvu1998 <congvinhvu98@gmail.com>
2019-10-26 14:16:12 -0400

    Creation of Sign Up Activity

---
+++ Programming/app/src/main/java/com/project/coen_elec_390/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.project.coen_elec_390;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
