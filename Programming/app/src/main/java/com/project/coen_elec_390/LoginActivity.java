bb992f4670c25dcd63823c1bed8dc2148cdca333
cvu1998 <congvinhvu98@gmail.com>
2019-10-27 13:55:19 -0400

    First Commit

--- Programming/app/src/main/java/com/project/coen_elec_390/LoginActivity.java
+++ Programming/app/src/main/java/com/project/coen_elec_390/LoginActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 public class LoginActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreference;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
