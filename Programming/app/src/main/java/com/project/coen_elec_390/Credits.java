2c837b63f6f7c2c82f634d0002eb75337aac5ee8
Olasubulumi Ogo-Oluwa Jesutomi <33330651+tomilashy@users.noreply.github.com>
2019-11-13 18:19:52 -0500

    Revert "added credits,deleted unused pics"
    This reverts commit 417c2378f1709d8ec721b4550645248af5c29754.

--- Programming/app/src/main/java/com/project/coen_elec_390/Credits.java
+++ Programming/app/src/main/java/com/project/coen_elec_390/Credits.java
@@ -2,56 +2,22 @@ package com.project.coen_elec_390;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.view.Display;
+import android.view.animation.Animation;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Credits extends AppCompatActivity {
-
-    ArrayAdapter<String> shadiAdapter,alecAdapter,irfanAdapter,tomiAdapter,cvuAdapter;
-    ArrayAdapter adapter2;
-    List<String> shadiList = new ArrayList<>();
-    List<String> alecList = new ArrayList<>();
-    List<String> irfanList = new ArrayList<>();
-    List<String> tomiList = new ArrayList<>();
-    List<String> cvuList = new ArrayList<>();
-    ListView shadiListView,alecListView,irfanListView,tomiListView,cvuListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
         setTitle("Credits");
-        shadiList.addAll(Arrays.asList(new String[]{"Shadi Makdissi","Project Leader"}));
-        alecList.addAll(Arrays.asList(new String[]{ "Alec Kurkdijan","Developer"}));
-        irfanList.addAll(Arrays.asList(new String[]{"Irfan Ahmed","Developer" }));
-        tomiList.addAll(Arrays.asList(new String[]{"Olasubulumi Jesutomi","Developer"}));
-        cvuList.addAll(Arrays.asList(new String[]{"Cong-Vinh Vu","Developer" }));
-//        setting adapter
-        shadiAdapter = new ArrayAdapter<String>(this, R.layout.listview2, shadiList);
-        alecAdapter = new ArrayAdapter<String>(this, R.layout.listview2, alecList);
-        irfanAdapter = new ArrayAdapter<String>(this, R.layout.listview2, irfanList);
-        tomiAdapter = new ArrayAdapter<String>(this, R.layout.listview2, tomiList);
-        cvuAdapter = new ArrayAdapter<String>(this, R.layout.listview2, cvuList);
-//        displaying list view
-
-        shadiListView = findViewById(R.id.shadiListview);
-        shadiListView.setAdapter(shadiAdapter);
-        alecListView = findViewById(R.id.alecListview);
-        alecListView.setAdapter(alecAdapter);
-        irfanListView = findViewById(R.id.irfanListview);
-        irfanListView.setAdapter(irfanAdapter);
-        tomiListView = findViewById(R.id.tomiListview);
-        tomiListView.setAdapter(tomiAdapter);
-        cvuListView = findViewById(R.id.cvuListview);
-        cvuListView.setAdapter(cvuAdapter);
-
     }
-
-
 }
